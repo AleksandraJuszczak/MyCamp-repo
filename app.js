e6ee4e6ddf71c134ae828a21bceb1e6dcdb0a82e
AleksandraJuszczak <a.juszczak@gmx.at>
2022-06-06 16:24:49 +0200

    adding the reviews model

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Joi = require('joi');
-const {campgroundSchema} = require('./schemas.js')
-const catchAsync = require('./utils/catchAsync')
-const ExpressError = require('./utils/ExpressError')
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const { nextTick } = require('process');
-const { render } = require('express/lib/response');
+const Review = require('./models/review');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 // bind() lets you create a new function with its this value set to a provided value
 // Because browser implementations require that console.error()’s this value be set to
@@ -33,10 +32,19 @@ app.use(methodOverride('_method'))
 
 const validateCampground = (req,res,next)=>{
     const {error} = campgroundSchema.validate(req.body);
-
     if(error){
-        const msg = error.details.map(el=> el.message).join('');
+        const msg = error.details.map(el=> el.message).join(',')
         throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req,res,next)=>{
+    const {error} =reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',');
+        throw new ExpressError(msg,400)
     } else {
         next();
     }
@@ -56,15 +64,15 @@ app.get('/campgrounds/new',(req,res)=>{
 })
 
 app.post('/campgrounds', validateCampground ,catchAsync(async(req,res,next)=>{
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
-    
-    const campground = new Campground(req.body.campgrounds);
+    // if(!req.body.campgrounds) throw new ExpressError('Invalid Campground Data',400);
+    const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show',{campground})
 }))
 
@@ -75,7 +83,7 @@ app.get('/campgrounds/:id/edit',catchAsync(async(req,res)=>{
 
 app.put('/campgrounds/:id', validateCampground, catchAsync(async(req,res)=>{
     const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campgrounds});
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
@@ -85,13 +93,30 @@ app.delete('/campgrounds/:id', catchAsync(async(req,res)=>{
     res.redirect('/campgrounds');
 }))
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res)=>{
+   const campground = await Campground.findById(req.params.id);
+   console.log(campground);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 app.all('*',(req,res,next)=>{
     next( new ExpressError('Page not found',404))
 })
 
 app.use((err,req,res,next)=>{
     const {statusCode=500}= err;
-    if(!err.message) err.message ='Oh No, Something Went Wrong!'
+    if(!err.message) err.message ='Oh No, Something Went Wrong!';
     res.status(statusCode).render('error',{err})
 })
 
